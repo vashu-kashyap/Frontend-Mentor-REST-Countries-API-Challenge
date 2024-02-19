14cee74d1e732f5a130a898167c71d00c217d14f
Vashu <im.vashukashyap@gmail.com>
2024-02-19 23:04:32 +0530

    Added Header component without theme switcher functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
 import "./globals.css";
-import {Nunito_Sans} from "next/font/google"
+import { Nunito_Sans } from "next/font/google";
+import Header from "@/components/Header";
 
 const nunitoSans = Nunito_Sans({
-  subsets:["latin"],
-  weight:["300","600","800"]
-})
+  subsets: ["latin"],
+  weight: ["300", "600", "800"],
+});
 
 export const metadata = {
   title: "Frontend Mentor | REST Countries API with color theme switcher",
@@ -14,7 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={nunitoSans.className}>{children}</body>
+      <body
+        className={`${nunitoSans.className} bg-very-light-gray dark:bg-very-dark-blue`}
+      >
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { MdOutlineDarkMode } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <header className="logo dark:bg-dark-blue flex items-center justify-between bg-white px-4 py-8 lg:px-20 ">
+      <div>
+        <Link className="text-dark font-extrabold dark:text-white " href={"/"}>
+          Where is in the World?
+        </Link>
+      </div>
+      <div className="theme-switcher">
+        <button className="flex items-center justify-center gap-2 font-semibold dark:text-white">
+          <MdOutlineDarkMode /> Dark Mode
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
